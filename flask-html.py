7e4327805cbe64ee47852a787627a300c0046689
crisrib <103081260+crisrib@users.noreply.github.com>
2022-10-26 07:14:30 -0400

    complete

---
+++ flask-html.py
@@ -0,0 +1,15 @@
+# Import packages
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template ('home_css.html')
+
+@app.route('/page2')
+def dashboard():
+    return render_template('page2_css.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host= '0.0.0.0', port=80)
